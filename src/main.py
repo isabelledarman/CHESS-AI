c672bf11ad7888a68375f8e1f735fb42171a225e
isabelledarman <135927120+isabelledarman@users.noreply.github.com>
2024-06-25 09:22:27 -0400

    beginning chess ai

---
+++ src/main.py
@@ -0,0 +1,51 @@
+import pygame
+import sys
+
+from const import *
+from game import Game
+
+
+class Main:
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('CHESS')
+        self.game = Game()
+
+    def mainloop(self):
+
+        screen = self.screen
+        game = self.game
+        board = self.game.board
+        dragger = self.game.dragger
+        
+        while True:
+            game.show_bg(screen)
+            game.show_pieces(screen)
+            for event in pygame.event.get():
+
+                if event.tupe == pygame.MOUSEBUTTONDOWN:
+                    dragger.update_mouse(event.pos)
+                    
+                    clicked_row = dragger.mouseX // SQSIZE
+                    clicked_col = dragger.mouseY //SQSIZE
+
+                    if board.square[clicked_row][clicked_col].has_piece():
+                        pass
+
+                elif event.type == pygame.MOUSEMOTION:
+                    pass
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    pass
+
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            
+
+            pygame.display.update()
+
+main = Main()
+main.mainloop()
